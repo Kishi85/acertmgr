53c4914db612e8bbcabeb890fec58de647857f62
Kishi85 <git@eightyfive.net>
2019-01-14 18:24:23 +0100

    dns.nsupdate: alternatively allow key data to be read from TSIG keyfile

--- modes/dns/nsupdate.py
+++ modes/dns/nsupdate.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 # dns.nsupdate - rfc2136 based challenge handler
 # Copyright (c) Rudolf Mayerhofer, 2018-2019
 # available under the ISC license, see LICENSE
+import re
 import time
 
 import dns.query
@@ -14,6 +14,33 @@ from modes.abstract import AbstractChallengeHandler
 
 
 class ChallengeHandler(AbstractChallengeHandler):
+    @staticmethod
+    def _read_tsigkey(tsig_key_file, key_name):
+        try:
+            key_file = open(tsig_key_file)
+            key_struct = key_file.read()
+            key_file.close()
+        except IOError as exc:
+            raise Exception(
+                "A problem was encountered opening your keyfile, %s." % tsig_key_file) from exc
+
+        try:
+            key_data = re.search(r"key \"%s\" \{(.*?)\}\;" % key_name, key_struct, re.DOTALL).group(1)
+            algorithm = re.search(r"algorithm ([a-zA-Z0-9_-]+?)\;", key_data, re.DOTALL).group(1)
+            tsig_secret = re.search(r"secret \"(.*?)\"", key_data, re.DOTALL).group(1)
+        except AttributeError as exc:
+            raise Exception(
+                "Unable to decipher the keyname and secret from your key file.") from exc
+
+        keyring = dns.tsigkeyring.from_text({
+            key_name: tsig_secret
+        })
+
+        if not algorithm:
+            algorithm = "HMAC-MD5.SIG-ALG.REG.INT"
+
+        return keyring, algorithm
+
     @staticmethod
     def _get_soa(domain, nameserver=None):
         if nameserver:
@@ -52,9 +79,14 @@ class ChallengeHandler(AbstractChallengeHandler):
 
     def __init__(self, config):
         AbstractChallengeHandler.__init__(self, config)
-        self.keyring = dns.tsigkeyring.from_text({
-            config.get("nsupdate_keyname"): config.get("nsupdate_keyvalue")
-        })
+        if 'nsupdate_keyfile' in config:
+            self.keyring, self.keyalgorithm = self._read_tsigkey(config.get("nsupdate_keyfile"),
+                                                                 config.get("nsupdate_keyname"))
+        else:
+            self.keyring = dns.tsigkeyring.from_text({
+                config.get("nsupdate_keyname"): config.get("nsupdate_keyvalue")
+            })
+            self.keyalgorithm = config.get("nsupdate_keyalgorithm", "HMAC-MD5.SIG-ALG.REG.INT")
         self.dns_server = config.get("nsupdate_server")
         self.dns_ttl = int(config.get("nsupdate_ttl", "60"))
         self.dns_updatedomain = config.get("nsupdate_updatedomain")
@@ -73,7 +105,7 @@ class ChallengeHandler(AbstractChallengeHandler):
         else:
             zone, nameserver = self._get_soa(domain)
 
-        update = dns.update.Update(zone, keyring=self.keyring)
+        update = dns.update.Update(zone, keyring=self.keyring, keyalgorithm=self.keyalgorithm)
         update.add(domain, self.dns_ttl, 'TXT', token)
         dns.query.tcp(update, nameserver)
         # Delay the challenge creation to allow dns changes to propagate
@@ -86,6 +118,6 @@ class ChallengeHandler(AbstractChallengeHandler):
             zone, _ = self._get_soa(domain, nameserver)
         else:
             zone, nameserver = self._get_soa(domain)
-        update = dns.update.Update(zone, keyring=self.keyring)
+        update = dns.update.Update(zone, keyring=self.keyring, keyalgorithm=self.keyalgorithm)
         update.delete(domain, self.dns_ttl, 'TXT', token)
         dns.query.tcp(update, nameserver)
