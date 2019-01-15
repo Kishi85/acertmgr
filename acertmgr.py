1791b5ce6c3fece73960b5dbbe838ca49ebd147f
Jan-Jonas Sämann <jan-jonas.saemann@saenet.de>
2019-01-15 23:25:15 +0100

    Fixed config file read routine exception and seek
      * Fixed exception of exception json.JSONDecodeError not found
        JSONDecodeError is a subclass of ValueError and is consistantly
        available in python2.7 and >3
      * seek config_fd back to begin before try to read handle as yaml

--- acertmgr.py
+++ acertmgr.py
@@ -205,10 +205,12 @@ if __name__ == "__main__":
                 import json
 
                 config = json.load(config_fd)
-            except json.JSONDecodeError:
+            except ValueError:
                 import yaml
 
+                config_fd.seek(0)
                 config = yaml.load(config_fd)
+
     if 'defaults' not in config:
         config['defaults'] = {}
     if 'server_key' not in config:
@@ -226,9 +228,10 @@ if __name__ == "__main__":
 
                     for entry in json.load(config_fd).items():
                         config['domains'].append(entry)
-                except json.JSONDecodeError:
+                except ValueError:
                     import yaml
 
+                    config_fd.seek(0)
                     for entry in yaml.load(config_fd).items():
                         config['domains'].append(entry)
 
